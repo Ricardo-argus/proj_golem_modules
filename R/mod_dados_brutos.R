ab8499f8b5d60705ee268b8c62b26d65d9ca8ef9
Ricardo-hernandes <ricardo.hernandes@argusmedia.com>
2025-10-31 22:59:31 -0300

    add new tabsets on Dados Completos

--- R/mod_dados_brutos.R
+++ R/mod_dados_brutos.R
@@ -10,14 +10,46 @@ mod_dados_brutos_ui <- function(id){
   tagList(
     fluidRow(
       box(
-        title = "Dados Consolidados dos Bolsistas", status = "primary", solidHeader = TRUE,
+        title = "Dados Consolidados dos Beneficiários", status = "primary", solidHeader = TRUE,
         width = 12,
-        DT::dataTableOutput(ns("tabela_completa"))
+
+        tabsetPanel(
+          id = ns("tabset_dados"),
+
+          tabPanel(
+            title = "PROUNI",
+            icon = icon("table"),
+
+            DT::dataTableOutput(ns("PROUNI"))
+
+          ),
+            # Aba 2: Exemplo de outra aba
+            tabPanel(
+              title = "BOLSA FAMÍLIA", # Título da segunda aba
+              icon = icon("table"), # Ícone opcional
+
+              # Você pode adicionar outros outputs aqui, como gráficos ou resumos
+              h4("Tabela a ser Preenchida."),
+              p("Adicionar dados dos Beneficiários do Bolsa Família."),
+              verbatimTextOutput(ns("resumo_exemplo")) # Exemplo de novo output
+            ),
+
+          tabPanel(
+            title = "Luz Para Todos",
+            icon = icon("table"),
+
+            h4("Tabela a ser preenchida."),
+            p("Adicionar dados dos Domicílios atendidos pelo Luz para Todos."),
+            verbatimTextOutput(ns("exemplo_resumo"))
+          )
+
+          )
+
+
+        )
       )
     )
-  )
 }
-
 #' mod_dados_brutos Server Function
 #'
 #' @description Server para a aba de Dados Completos.
@@ -31,27 +63,10 @@ mod_dados_brutos_server <- function(id, dados_filtrados){
   moduleServer( id, function(input, output, session){
     ns <- session$ns
 
-    output$tabela_completa <- DT::renderDataTable({
+    output$PROUNI <- DT::renderDataTable({
       df <- dados_filtrados()
       req(nrow(df) > 0)
 
-      colunas_para_dropdown <- c(
-        "SEXO_BENEFICIARIO",
-        "RACA_BENEFICIARIO",
-        "UF_BENEFICIARIO"
-      )
-
-      colunas_existentes <- intersect(colunas_para_dropdown, names(df))
-
-      if (length(colunas_existentes) > 0) {
-        df <- dplyr::mutate(
-          df,
-          dplyr::across(
-            dplyr::all_of(colunas_existentes),
-            as.factor
-          )
-        )
-      }
 
       DT::datatable(
         df,
